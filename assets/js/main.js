5442c2e07b2f29606b02a1eb9cedf04f903115ce
Igor Cunha <igorcferreiraa@gmail.com>
2022-01-15 17:48:23 -0300

    terceiro commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,54 @@
+function converteCpf(umCpf){
+    let cpfString = umCpf.value;
+    let cpfNumber;
+
+    console.log(cpfString);
+
+    cpfString = cpfString.replace(/\./g, '');
+    cpfString = cpfString.replace(/\-/, '');
+
+    if (cpfString.length != 11){
+        alert('CPF inálido');
+    }
+
+    cpfNumber = Number(cpfString);
+    console.log(cpfNumber);
+    return cpfNumber;
+}
+
+function formataData(umaData){
+
+    let data = new Date(umaData.value);
+
+    data = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+
+    //let dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+
+    console.log(data);
+    return data;
+
+}
+
+function convertePeso(umPeso){
+
+    let peso = umPeso.value;
+    peso = Number(peso);
+
+    console.log(peso);
+
+    return peso;
+
+}
+
+function converteAltura(umaAltura){
+
+    let altura = umaAltura.value;
+    altura = Number(altura);
+
+    console.log(altura);
+
+}
+
 function meuFormulario(){
     const form = document.querySelector('.formulario');
     const resultado = document.querySelector('.resultado');
@@ -10,10 +61,15 @@ function meuFormulario(){
         const peso = evento.target.querySelector('#peso');
         const altura = evento.target.querySelector('#altura');
         const uf = evento.target.querySelector('#uf');
-        console.log(nome.value, cpf.value, data.value, peso.value, altura.value, uf.value);
 
+        formataData(data);
+        convertePeso(peso);
+        converteAltura(altura);
+
+        console.log(nome.value, cpf.value, data.value, peso.value, altura.value, uf.value, typeof(uf.value));
         resultado.innerHTML = ` `;
-        resultado.innerHTML += `<p>Meu nome é ${nome.value} portador(a) do CPF: ${cpf.value}, meu peso e altura são: ${peso.value}kg e ${altura.value}m. Sou residente do estado de ${uf.value} e minha data de nascimento é: ${data.value}</p>`;
+        resultado.innerHTML += `<p>Meu nome é ${nome.value} portador(a) do CPF: ${cpf.value}, meu peso e altura são: ${peso.value}kg e ${altura.value}m. Sou residente do estado de ${uf.value} e minha data de nascimento é: ${formataData(data)}</p>`;
     }
 }
+
 meuFormulario();
